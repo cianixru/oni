cianixru/oni

commit d23e8077ae5a2add86a5c24a0c616d786e6321fa

Refactoring: Move 'Menu' to use 'Overlay' API (#1356)
* Refactor Menu/Tasks to explicit dependencies

* Refactor menu to create overlay to host

* Fix lint issues

browser/src/Editor/NeovimEditor/NeovimEditor.tsx
@@ -50,7 +50,8 @@ import {
     SyntaxHighlighter,
 } from "./../../Services/SyntaxHighlighting"
 
-import { tasks } from "./../../Services/Tasks"
+import { MenuManager } from "./../../Services/Menu"
+import { Tasks } from "./../../Services/Tasks"
 import { ThemeManager } from "./../../Services/Themes"
 import { TypingPredictionManager } from "./../../Services/TypingPredictionManager"
 import { Workspace } from "./../../Services/Workspace"
@@ -146,7 +147,9 @@ export class NeovimEditor extends Editor implements IEditor {
         private _configuration: Configuration,
         private _diagnostics: IDiagnosticsDataSource,
         private _languageManager: LanguageManager,
+        private _menuManager: MenuManager,
         private _pluginManager: PluginManager,
+        private _tasks: Tasks,
         private _themeManager: ThemeManager,
         private _workspace: Workspace,
     ) {
@@ -169,7 +172,7 @@ export class NeovimEditor extends Editor implements IEditor {
         this._hoverRenderer = new HoverRenderer(this._colors, this, this._configuration, this._toolTipsProvider)
 
         this._definition = new Definition(this, this._store)
-        this._symbols = new Symbols(this, this._definition, this._languageManager)
+        this._symbols = new Symbols(this, this._definition, this._languageManager, this._menuManager)
 
         this._diagnostics.onErrorsChanged.subscribe(() => {
             const errors = this._diagnostics.getErrors()
@@ -197,6 +200,7 @@ export class NeovimEditor extends Editor implements IEditor {
             this._contextMenuManager,
             this._definition,
             this._languageIntegration,
+            this._menuManager,
             this._neovimInstance,
             this._rename,
             this._symbols,
@@ -211,8 +215,8 @@ export class NeovimEditor extends Editor implements IEditor {
         window.addEventListener("resize", updateViewport)
         updateViewport()
 
-        tasks.registerTaskProvider(commandManager)
-        tasks.registerTaskProvider(errorService)
+        this._tasks.registerTaskProvider(commandManager)
+        this._tasks.registerTaskProvider(errorService)
 
         services.push(errorService)
 
@@ -457,16 +461,6 @@ export class NeovimEditor extends Editor implements IEditor {
         this._languageIntegration.onHideDefinition.subscribe((definition) => {
             this._actions.hideDefinition()
         })
-
-        this._commands = new NeovimEditorCommands(
-            commandManager,
-            this._contextMenuManager,
-            this._definition,
-            this._languageIntegration,
-            this._neovimInstance,
-            this._rename,
-            this._symbols,
-        )
 
         this._render()
 

browser/src/Editor/OniEditor/OniEditor.tsx
@@ -26,10 +26,13 @@ import {
     LanguageManager,
 } from "./../../Services/Language"
 
+import { MenuManager } from "./../../Services/Menu"
+
 import {
     ISyntaxHighlighter,
 } from "./../../Services/SyntaxHighlighting"
 
+import { Tasks } from "./../../Services/Tasks"
 import { ThemeManager } from "./../../Services/Themes"
 import { Workspace } from "./../../Services/Workspace"
 
@@ -104,11 +107,13 @@ export class OniEditor implements IEditor {
          configuration: Configuration,
          diagnostics: IDiagnosticsDataSource,
          languageManager: LanguageManager,
+         menuManager: MenuManager,
          pluginManager: PluginManager,
+         tasks: Tasks,
          themeManager: ThemeManager,
          workspace: Workspace,
     ) {
-        this._neovimEditor = new NeovimEditor(colors, completionProviders, configuration, diagnostics, languageManager, pluginManager, themeManager, workspace)
+        this._neovimEditor = new NeovimEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, themeManager, workspace)
 
         this._neovimEditor.bufferLayers.addBufferLayer("*", (buf) => wrapReactComponentWithLayer("oni.layer.scrollbar", <BufferScrollBarContainer />))
         this._neovimEditor.bufferLayers.addBufferLayer("*", (buf) => wrapReactComponentWithLayer("oni.layer.definition", <DefinitionContainer />))

browser/src/index.tsx
@@ -29,6 +29,8 @@ const start = async (args: string[]): Promise<void> => {
     const statusBarPromise = import("./Services/StatusBar")
     const startEditorsPromise = import("./startEditors")
 
+    const menuPromise = import("./Services/Menu")
+
     const sharedNeovimInstancePromise = import("./neovim/SharedNeovimInstance")
     const autoClosingPairsPromise = import("./Services/AutoClosingPairs")
     const browserWindowConfigurationSynchronizerPromise = import("./Services/BrowserWindowConfigurationSynchronizer")
@@ -39,6 +41,7 @@ const start = async (args: string[]): Promise<void> => {
     const inputManagerPromise = import("./Services/InputManager")
     const languageManagerPromise = import("./Services/Language")
     const snippetPromise = import("./Services/Snippets")
+    const taksPromise = import("./Services/Tasks")
     const workspacePromise = import("./Services/Workspace")
 
     const themePickerPromise = import("./Services/Themes/ThemePicker")
@@ -113,6 +116,15 @@ const start = async (args: string[]): Promise<void> => {
 
     const Overlay = await overlayPromise
     Overlay.activate()
+    const overlayManager = Overlay.getInstance()
+
+    const Menu = await menuPromise
+    Menu.activate(overlayManager)
+    const menuManager = Menu.getInstance()
+
+    const Tasks = await taksPromise
+    Tasks.activate(menuManager)
+    const tasks = Tasks.getInstance()
 
     const LanguageManager = await languageManagerPromise
     LanguageManager.activate(configuration, editorManager, statusBar, workspace)
@@ -136,7 +148,7 @@ const start = async (args: string[]): Promise<void> => {
    await Promise.race([Utility.delay(5000),
      Promise.all([
         SharedNeovimInstance.activate(configuration, pluginManager),
-        startEditors(filesToOpen, Colors.getInstance(), CompletionProviders.getInstance(), configuration, diagnostics, languageManager, pluginManager, Themes.getThemeManagerInstance(), workspace)
+        startEditors(filesToOpen, Colors.getInstance(), CompletionProviders.getInstance(), configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, Themes.getThemeManagerInstance(), workspace)
     ])
    ])
     Performance.endMeasure("Oni.Start.Editors")
@@ -163,13 +175,13 @@ const start = async (args: string[]): Promise<void> => {
     AutoClosingPairs.activate(configuration, editorManager, inputManager, languageManager)
 
     const GlobalCommands = await globalCommandsPromise
-    GlobalCommands.activate(commandManager)
+    GlobalCommands.activate(commandManager, menuManager, tasks)
 
     const Snippets = await snippetPromise
     Snippets.activate()
 
     const ThemePicker = await themePickerPromise
-    ThemePicker.activate(configuration, Themes.getThemeManagerInstance())
+    ThemePicker.activate(configuration, menuManager, Themes.getThemeManagerInstance())
 
     Performance.endMeasure("Oni.Start.Activate")
 

browser/src/startEditors.ts
@@ -14,13 +14,15 @@ import { Configuration } from "./Services/Configuration"
 import { IDiagnosticsDataSource } from "./Services/Diagnostics"
 import { editorManager } from "./Services/EditorManager"
 import { LanguageManager } from "./Services/Language"
+import { MenuManager } from "./Services/Menu"
+import { Tasks } from "./Services/Tasks"
 import { ThemeManager } from "./Services/Themes"
 import { windowManager } from "./Services/WindowManager"
 import { Workspace } from "./Services/Workspace"
 
-export const startEditors = async (args: any, colors: Colors, completionProviders: CompletionProviders, configuration: Configuration, diagnostics: IDiagnosticsDataSource, languageManager: LanguageManager, pluginManager: PluginManager, themeManager: ThemeManager, workspace: Workspace): Promise<void> => {
+export const startEditors = async (args: any, colors: Colors, completionProviders: CompletionProviders, configuration: Configuration, diagnostics: IDiagnosticsDataSource, languageManager: LanguageManager, menuManager: MenuManager, pluginManager: PluginManager, tasks: Tasks, themeManager: ThemeManager, workspace: Workspace): Promise<void> => {
 
-    const editor = new OniEditor(colors, completionProviders, configuration, diagnostics, languageManager, pluginManager, themeManager, workspace)
+    const editor = new OniEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, themeManager, workspace)
     editorManager.setActiveEditor(editor)
     windowManager.split(0, editor)
 

browser/src/Editor/NeovimEditor/NeovimEditorCommands.ts
@@ -17,7 +17,7 @@ import { getUserConfigFilePath } from "./../../Services/Configuration"
 import { ContextMenuManager } from "./../../Services/ContextMenu"
 import { editorManager } from "./../../Services/EditorManager"
 import { findAllReferences, format, LanguageEditorIntegration } from "./../../Services/Language"
-import { menuManager } from "./../../Services/Menu"
+import { MenuManager } from "./../../Services/Menu"
 import { QuickOpen } from "./../../Services/QuickOpen"
 import { replaceAll } from "./../../Utility"
 
@@ -34,6 +34,7 @@ export class NeovimEditorCommands {
         private _contextMenuManager: ContextMenuManager,
         private _definition: Definition,
         private _languageEditorIntegration: LanguageEditorIntegration,
+        private _menuManager: MenuManager,
         private _neovimInstance: NeovimInstance,
         private _rename: Rename,
         private _symbols: Symbols,
@@ -45,7 +46,7 @@ export class NeovimEditorCommands {
         // TODO: This should be extracted
         // - Should not depend on NeovimInstance
         // - Should be able to work against the public 'IEditor' interface
-        const quickOpen = new QuickOpen(this._neovimInstance)
+        const quickOpen = new QuickOpen(this._menuManager, this._neovimInstance)
 
         const quickOpenCommand = (innerCommand: Oni.Commands.CommandCallback) => (qo: QuickOpen) => {
             return () => {
@@ -116,7 +117,7 @@ export class NeovimEditorCommands {
     }
 
         const shouldShowMenu = () => {
-            return !menuManager.isMenuOpen()
+            return !this._menuManager.isMenuOpen()
         }
 
         const openFolder = (neovimInstance: NeovimInstance) => {

browser/src/Editor/NeovimEditor/Symbols.ts
@@ -7,7 +7,7 @@ import * as types from "vscode-languageserver-types"
 
 import * as Oni from "oni-api"
 
-import { menuManager } from "./../../Services/Menu"
+import { MenuManager } from "./../../Services/Menu"
 
 import { LanguageManager } from "./../../Services/Language"
 
@@ -21,12 +21,13 @@ export class Symbols {
         private _editor: Oni.Editor,
         private _definition: Definition,
         private _languageManager: LanguageManager,
+        private _menuManager: MenuManager,
     ) {
 
     }
 
     public async openWorkspaceSymbolsMenu() {
-        const menu = menuManager.create()
+        const menu = this._menuManager.create()
 
         menu.show()
         menu.setItems([{
@@ -79,7 +80,7 @@ export class Symbols {
     }
 
     public async openDocumentSymbolsMenu(): Promise<void> {
-        const menu = menuManager.create()
+        const menu = this._menuManager.create()
 
         menu.show()
         menu.setLoading(true)

browser/src/Plugins/Api/Oni.ts
@@ -23,7 +23,7 @@ import { getInstance as getDiagnosticsInstance } from "./../../Services/Diagnost
 import { editorManager } from "./../../Services/EditorManager"
 import { inputManager } from "./../../Services/InputManager"
 import * as LanguageManager from "./../../Services/Language"
-import { menuManager } from "./../../Services/Menu"
+import { getInstance as getMenuManagerInstance } from "./../../Services/Menu"
 import { getInstance as getOverlayInstance } from "./../../Services/Overlay"
 import { recorder } from "./../../Services/Recorder"
 import { getInstance as getSidebarInstance } from "./../../Services/Sidebar"
@@ -111,7 +111,7 @@ export class Oni extends EventEmitter implements OniApi.Plugin.Api {
     }
 
     public get menu(): any /* TODO */ {
-        return menuManager
+        return getMenuManagerInstance()
     }
 
     public get overlays(): any /* TODO */ {

browser/src/Services/Commands/GlobalCommands.ts
@@ -9,10 +9,10 @@ import { remote } from "electron"
 
 import * as Oni from "oni-api"
 
-import { menuManager } from "./../../Services/Menu"
+import { MenuManager } from "./../../Services/Menu"
 import { showAboutMessage } from "./../../Services/Metadata"
 import { multiProcess } from "./../../Services/MultiProcess"
-import { tasks } from "./../../Services/Tasks"
+import { Tasks } from "./../../Services/Tasks"
 import { windowManager } from "./../../Services/WindowManager"
 
 // import * as UI from "./../UI/index"
@@ -21,7 +21,22 @@ import { CallbackCommand, CommandManager } from "./../CommandManager"
 
 import * as Platform from "./../../Platform"
 
-export const activate = (commandManager: CommandManager) => {
+export const activate = (commandManager: CommandManager, menuManager: MenuManager, tasks: Tasks) => {
+
+    const popupMenuCommand = (innerCommand: Oni.Commands.CommandCallback) => {
+        return () => {
+            if (menuManager.isMenuOpen()) {
+                return innerCommand()
+            }
+
+            return false
+        }
+    }
+
+    const popupMenuClose = popupMenuCommand(() => menuManager.closeActiveMenu())
+    const popupMenuNext = popupMenuCommand(() => menuManager.nextMenuItem())
+    const popupMenuPrevious = popupMenuCommand(() => menuManager.previousMenuItem())
+    const popupMenuSelect = popupMenuCommand(() => menuManager.selectMenuItem())
 
     const commands = [
 
@@ -75,18 +90,3 @@ export const activate = (commandManager: CommandManager) => {
 
     commands.forEach((c) => commandManager.registerCommand(c))
 }
-
-const popupMenuCommand = (innerCommand: Oni.Commands.CommandCallback) => {
-    return () => {
-        if (menuManager.isMenuOpen()) {
-            return innerCommand()
-        }
-
-        return false
-    }
-}
-
-const popupMenuClose = popupMenuCommand(() => menuManager.closeActiveMenu())
-const popupMenuNext = popupMenuCommand(() => menuManager.nextMenuItem())
-const popupMenuPrevious = popupMenuCommand(() => menuManager.previousMenuItem())
-const popupMenuSelect = popupMenuCommand(() => menuManager.selectMenuItem())

commit 423e12cd139844b34d54365fe8880c78bfc3922a

Regression: Fix external command line that was broken by #1356 (#1357)
* Fix regression in external commandline after the change to move Menu -> Overlay

* Fix lint issues

* Add helper class for command line box, for automation

* Fix issue with wildmenu causing commandline to hide

* Add test to validate command line gets shown

* Fix type in test name

browser/src/Editor/NeovimEditor/NeovimEditor.tsx
@@ -36,6 +36,7 @@ import { Completion, CompletionProviders } from "./../../Services/Completion"
 import { Configuration, IConfigurationValues } from "./../../Services/Configuration"
 import { IDiagnosticsDataSource } from "./../../Services/Diagnostics"
 import { Errors } from "./../../Services/Errors"
+import { Overlay, OverlayManager } from "./../../Services/Overlay"
 import * as Shell from "./../../UI/Shell"
 
 import {
@@ -79,6 +80,10 @@ import { Rename } from "./Rename"
 import { Symbols } from "./Symbols"
 import { IToolTipsProvider, NeovimEditorToolTipsProvider } from "./ToolTipsProvider"
 
+import CommandLine from "./../../UI/components/CommandLine"
+import ExternalMenus from "./../../UI/components/ExternalMenus"
+import WildMenu from "./../../UI/components/WildMenu"
+
 import { WelcomeBufferLayer } from "./WelcomeBufferLayer"
 
 export class NeovimEditor extends Editor implements IEditor {
@@ -121,6 +126,7 @@ export class NeovimEditor extends Editor implements IEditor {
     private _definition: Definition = null
     private _toolTipsProvider: IToolTipsProvider
     private _commands: NeovimEditorCommands
+    private _externalMenuOverlay: Overlay
 
     private _bufferLayerManager: BufferLayerManager
 
@@ -148,6 +154,7 @@ export class NeovimEditor extends Editor implements IEditor {
         private _diagnostics: IDiagnosticsDataSource,
         private _languageManager: LanguageManager,
         private _menuManager: MenuManager,
+        private _overlayManager: OverlayManager,
         private _pluginManager: PluginManager,
         private _tasks: Tasks,
         private _themeManager: ThemeManager,
@@ -178,6 +185,15 @@ export class NeovimEditor extends Editor implements IEditor {
             const errors = this._diagnostics.getErrors()
             this._actions.setErrors(errors)
         })
+
+        this._externalMenuOverlay = this._overlayManager.createItem()
+        this._externalMenuOverlay.setContents(
+                <Provider store={this._store}>
+                    <ExternalMenus>
+                        <CommandLine />
+                        <WildMenu />
+                    </ExternalMenus>
+                </Provider>)
 
         this._popupMenu = new NeovimPopupMenu(
             this._neovimInstance.onShowPopupMenu,
@@ -241,6 +257,7 @@ export class NeovimEditor extends Editor implements IEditor {
                 showCommandLineInfo.indent,
                 showCommandLineInfo.level,
             )
+            this._externalMenuOverlay.show()
         })
 
         this._neovimInstance.onWildMenuShow.subscribe(wildMenuInfo => {
@@ -251,10 +268,13 @@ export class NeovimEditor extends Editor implements IEditor {
             this._actions.wildMenuSelect(wildMenuInfo)
         })
 
-        this._neovimInstance.onWildMenuHide.subscribe(this._actions.hideWildMenu)
+        this._neovimInstance.onWildMenuHide.subscribe(() => {
+            this._actions.hideWildMenu()
+        })
 
         this._neovimInstance.onCommandLineHide.subscribe(() => {
             this._actions.hideCommandLine()
+            this._externalMenuOverlay.hide()
         })
 
         this._neovimInstance.onCommandLineSetCursorPosition.subscribe(commandLinePos => {

browser/src/Editor/NeovimEditor/NeovimSurface.tsx
@@ -11,15 +11,12 @@ import { IEvent } from "oni-types"
 import { NeovimInstance, NeovimScreen } from "./../../neovim"
 import { INeovimRenderer } from "./../../Renderer"
 
-import CommandLine from "./../../UI/components/CommandLine"
 import { Cursor } from "./../../UI/components/Cursor"
 import { CursorLine } from "./../../UI/components/CursorLine"
-import ExternalMenus from "./../../UI/components/ExternalMenus"
 import { InstallHelp } from "./../../UI/components/InstallHelp"
 import { TabsContainer } from "./../../UI/components/Tabs"
 import { ToolTips } from "./../../UI/components/ToolTip"
 import { TypingPrediction } from "./../../UI/components/TypingPredictions"
-import WildMenu from "./../../UI/components/WildMenu"
 
 import { TypingPredictionManager } from "./../../Services/TypingPredictionManager"
 
@@ -58,10 +55,6 @@ export class NeovimSurface extends React.PureComponent<INeovimSurfaceProps, {}> 
                     onTabSelect={this.props.onTabSelect}/>
             </div>
             <div className="container full">
-                <ExternalMenus>
-                    <CommandLine />
-                    <WildMenu />
-                </ExternalMenus>
                 <div className="stack">
                     <NeovimRenderer renderer={this.props.renderer}
                         neovimInstance={this.props.neovimInstance}

browser/src/Editor/OniEditor/OniEditor.tsx
@@ -27,6 +27,7 @@ import {
 } from "./../../Services/Language"
 
 import { MenuManager } from "./../../Services/Menu"
+import { OverlayManager } from "./../../Services/Overlay"
 
 import {
     ISyntaxHighlighter,
@@ -108,12 +109,13 @@ export class OniEditor implements IEditor {
          diagnostics: IDiagnosticsDataSource,
          languageManager: LanguageManager,
          menuManager: MenuManager,
+         overlayManager: OverlayManager,
          pluginManager: PluginManager,
          tasks: Tasks,
          themeManager: ThemeManager,
          workspace: Workspace,
     ) {
-        this._neovimEditor = new NeovimEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, themeManager, workspace)
+        this._neovimEditor = new NeovimEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, overlayManager, pluginManager, tasks, themeManager, workspace)
 
         this._neovimEditor.bufferLayers.addBufferLayer("*", (buf) => wrapReactComponentWithLayer("oni.layer.scrollbar", <BufferScrollBarContainer />))
         this._neovimEditor.bufferLayers.addBufferLayer("*", (buf) => wrapReactComponentWithLayer("oni.layer.definition", <DefinitionContainer />))

browser/src/UI/components/CommandLine.tsx
@@ -137,7 +137,7 @@ class CommandLine extends React.PureComponent<ICommandLineRendererProps, State> 
         return (
             !waiting &&
             visible && (
-                <CommandLineBox>
+                <CommandLineBox className="command-line">
                     <CommandLineOutput innerRef={e => (this._inputElement = e)}>
                         {this.renderIconOrChar(firstchar)}
                         {this.props.prompt}

browser/src/UI/components/ExternalMenus.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import * as ReactDOM from "react-dom"
 import { connect } from "react-redux"
 import styled from "styled-components"
 
@@ -28,23 +27,12 @@ interface Props {
 
 class ExternalMenus extends React.Component<Props> {
 
-    public constructor(props: Props, private stackLayer: HTMLDivElement) {
-        super(props)
-
-        this.stackLayer = document.querySelector(".stack .layer")
-    }
-
     public render() {
         const { wildmenu, commandLine } = this.props
         const visible = commandLine.visible || wildmenu.visible
-        return (
-            visible && ReactDOM.createPortal(
-                <MenuContainer loaded={commandLine.visible && wildmenu.visible}>
+        return visible ? <MenuContainer loaded={commandLine.visible && wildmenu.visible}>
                     {this.props.children}
-                </MenuContainer>,
-                this.stackLayer,
-            )
-        )
+                </MenuContainer> : null
     }
 }
 

browser/src/index.tsx
@@ -148,7 +148,7 @@ const start = async (args: string[]): Promise<void> => {
    await Promise.race([Utility.delay(5000),
      Promise.all([
         SharedNeovimInstance.activate(configuration, pluginManager),
-        startEditors(filesToOpen, Colors.getInstance(), CompletionProviders.getInstance(), configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, Themes.getThemeManagerInstance(), workspace)
+        startEditors(filesToOpen, Colors.getInstance(), CompletionProviders.getInstance(), configuration, diagnostics, languageManager, menuManager, overlayManager, pluginManager, tasks, Themes.getThemeManagerInstance(), workspace)
     ])
    ])
     Performance.endMeasure("Oni.Start.Editors")

browser/src/startEditors.ts
@@ -15,14 +15,15 @@ import { IDiagnosticsDataSource } from "./Services/Diagnostics"
 import { editorManager } from "./Services/EditorManager"
 import { LanguageManager } from "./Services/Language"
 import { MenuManager } from "./Services/Menu"
+import { OverlayManager } from "./Services/Overlay"
 import { Tasks } from "./Services/Tasks"
 import { ThemeManager } from "./Services/Themes"
 import { windowManager } from "./Services/WindowManager"
 import { Workspace } from "./Services/Workspace"
 
-export const startEditors = async (args: any, colors: Colors, completionProviders: CompletionProviders, configuration: Configuration, diagnostics: IDiagnosticsDataSource, languageManager: LanguageManager, menuManager: MenuManager, pluginManager: PluginManager, tasks: Tasks, themeManager: ThemeManager, workspace: Workspace): Promise<void> => {
+export const startEditors = async (args: any, colors: Colors, completionProviders: CompletionProviders, configuration: Configuration, diagnostics: IDiagnosticsDataSource, languageManager: LanguageManager, menuManager: MenuManager, overlayManager: OverlayManager, pluginManager: PluginManager, tasks: Tasks, themeManager: ThemeManager, workspace: Workspace): Promise<void> => {
 
-    const editor = new OniEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, pluginManager, tasks, themeManager, workspace)
+    const editor = new OniEditor(colors, completionProviders, configuration, diagnostics, languageManager, menuManager, overlayManager, pluginManager, tasks, themeManager, workspace)
     editorManager.setActiveEditor(editor)
     windowManager.split(0, editor)
 

test/CiTests.ts
@@ -17,6 +17,7 @@ const CiTests = [
     "AutoCompletionTest-CSS",
     "AutoCompletionTest-HTML",
     "AutoCompletionTest-TypeScript",
+    "Editor.ExternalCommandLineTest",
     "LargeFileTest",
     "PaintPerformanceTest",
     "QuickOpenTest",

test/ci/Editor.ExternalCommandLine.config.js
@@ -1,0 +1,7 @@
+// For more information on customizing Oni,
+// check out our wiki page:
+// https://github.com/onivim/oni/wiki/Configuration
+
+module.exports = {
+    "experimental.commandline.mode": true,
+};

test/ci/Editor.ExternalCommandLineTest.ts
@@ -1,0 +1,23 @@
+/**
+ * Test script to validate the external command line
+ */
+
+import * as assert from "assert"
+import * as Oni from "oni-api"
+
+import { getElementByClassName } from "./Common"
+
+export const test = async (oni: Oni.Plugin.Api) => {
+    await oni.automation.waitForEditors()
+
+    oni.automation.sendKeys(":")
+
+    await oni.automation.waitFor(() => !!getElementByClassName("command-line"))
+
+    assert.ok(!!getElementByClassName("command-line"), "Validate command line UI is shown")
+}
+
+// Bring in custom config to turn off animations, in order to reduce noise.
+export const settings = {
+    configPath: "Editor.ExternalCommandLine.config.js",
+}